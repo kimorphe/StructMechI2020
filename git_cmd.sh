f0993827678fa93bccd1c6de7e7b11ad75a1c5e2
kazushi <kimoto@okayama-u.ac.jp>
2020-09-24 15:44:39 +0900

    First commit

---
+++ git_cmd.sh
@@ -0,0 +1,23 @@
+git add A/Lec*/*.tex
+git add A/Lec*/*.svgz
+git add A/Lec*/makefile
+git add A/Exam*/*.tex
+git add A/Exam*/*.svgz
+git add A/Exam*/makefile
+
+git add B/Lec*/*.tex
+git add B/Lec*/*.svgz
+git add B/Lec*/makefile
+git add B/Exam*/*.tex
+git add B/Exam*/*.svgz
+git add B/Exam*/makefile
+
+git add Chap*/*.tex
+git add Chap*/*.svgz
+git add Chap*/makefile
+
+
+git add git_cmd.sh
+
+git commit
+git push -u origin master
